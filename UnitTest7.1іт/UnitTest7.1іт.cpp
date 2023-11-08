7cfe2dc8095f4f4f1ad986f74eb4fad821f37fb9
RitaPylypaka <ritapylypaka@gmail.com>
2023-11-08 16:13:05 +0200

    Add project files.

---
+++ UnitTest7.1іт/UnitTest7.1іт.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр7.1/пр7.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71іт
+{
+	TEST_CLASS(UnitTest71іт)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int colCount = 2;
+			int rowCount = 2;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+			a[0][0] = 6;
+			a[1][1] = 4;
+			a[0][1] = 12;
+			a[1][0] = 5;
+			int S = 0;
+			int k = 0;
+			Calc(a, rowCount, colCount, S, k);
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+			Assert::AreEqual(S, 12);
+
+
+		}
+	};
+}
